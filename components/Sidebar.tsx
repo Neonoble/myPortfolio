f1745d380a496eb57de0f346a9313addd39fa6b0
Neonoble <104133057+Neonoble@users.noreply.github.com>
2023-05-08 11:17:14 +0200

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -23,7 +23,7 @@ const Sidebar = () => {
             <button
                onClick={changeTheme}
                title='Theme'
-               className='hidden w-1 px-4 py-4 text-white rounded-full cursor-pointer bg-gradient-to-r from-green-400 to-blue-400 focus:outline-none hover:scale-105'>
+               className='w-1 px-4 py-4 text-white rounded-full cursor-pointer bg-gradient-to-r from-green-400 to-blue-400 focus:outline-none hover:scale-105'>
                {/* //TODO remove bg black */}                             
             </button>
          </section>         
