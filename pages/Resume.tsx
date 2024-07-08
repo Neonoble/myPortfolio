e2524fcb3ab8c75668da4828b72a9f55f441c062
Neonoble <104133057+Neonoble@users.noreply.github.com>
2024-07-08 23:56:17 +0200

    Update Resume.tsx

--- pages/Resume.tsx
+++ pages/Resume.tsx
@@ -6,28 +6,84 @@ import { languages, tools } from "../Data/data";
 const Resume = () => {
   return (
     <div className="px-6 py-2">
-      {/* //! Education & Experience */}
+      {/* //! Certificates & Experience */}
       <div className="grid gap-6 md:grid-cols-2">
+        
+       {/*Certs */}
         <div>
           <h5 className="my-3 text-2xl font-bold">Certificates</h5>
           <div className="">
-            <p className="font-semibold">Attended Oaklands high school (2017-2021)</p>
-            <p className="my-2">
-              I am currently working as a Web developer intern at Younglings Africa.
+            <p className="font-semibold">
+               <a style={{textDecoration:'none'} href='https://www.freecodecamp.org/certification/fcce91ce728-95f0-44c0-815f-2e0ff652952e/responsive-web-design'>
+                  Responsive Web Design
+              </a>
             </p>
           </div>
         </div>
-        <div>        
-          <h5 className="my-3 text-2xl font-bold">Experience</h5>         
-          <div className="scrollbar" id="style-14" style={{overflow:'auto',height:'150px'}}>
-           <div className="force-overflow">
-              <h5 className="my-2 text-xl font-bold">Web developer Intern</h5>
-              <p className="font-semibold">@Younglings Africa (April 2022 - present)</p>
-              <p className="my-3">Was tasked with creating the companies Alumni Portal, which I completed using React.js .</p>
-              <hr/>              
-           </div>                     
-          </div>          
+
+         <div>
+          <h5 className="my-3 text-2xl font-bold">Certificates</h5>
+          <div className="">
+            <p className="font-semibold">
+               <a style={{textDecoration:'none'} href='https://www.freecodecamp.org/certification/fcce91ce728-95f0-44c0-815f-2e0ff652952e/javascript-algorithms-and-data-structures'>
+                  JavaScript Algorithims and Date Structures
+              </a>
+            </p>
+          </div>
         </div>
+
+         <div>
+          <h5 className="my-3 text-2xl font-bold">Certificates</h5>
+          <div className="">
+            <p className="font-semibold">
+               <a style={{textDecoration:'none'} href='https://ix-certificate.tiiny.site/'>
+                  Ixperience Internship
+              </a>
+            </p>
+          </div>
+        </div>
+
+         <div>
+          <h5 className="my-3 text-2xl font-bold">Certificates</h5>
+          <div className="">
+            <p className="font-semibold">
+               <a style={{textDecoration:'none'} href='https://neomunhenga-hubbardcert.tiiny.site/'>
+                  Hubbard Communication Course
+              </a>
+            </p>
+          </div>
+        </div>
+
+      {/*Exp*/}
+        <div>        
+          <h5 className="my-3 text-2xl font-bold">Experience</h5>
+          
+          <div className="scrollbar" id="style-14" style={{overflow:'auto',height:'150px'}}>
+            
+           <div className="force-overflow">
+              <h5 className="my-2 text-xl font-bold">Software Developer Intern</h5>
+              <p className="font-semibold">@Younglings Africa (April 2022 - June 2023)</p>
+              <p className="my-3">Worked with the development team to implement features that met project requirements.</p>
+              <hr/>              
+           </div>         
+
+            <div className="force-overflow">
+              <h5 className="my-2 text-xl font-bold">Funnel Engineer</h5>
+              <p className="font-semibold">@Blacfox (July 2023 - March 2024)</p>
+              <p className="my-3">Contributed to marketing campaigns by building conversion-focused landing pages.</p>
+              <hr/>              
+           </div>     
+
+            <div className="force-overflow">
+              <h5 className="my-2 text-xl font-bold">Software Developer</h5>
+              <p className="font-semibold">Freelancer (April 2024)</p>
+              <p className="my-3">Working as a Software Developer freelancer.</p>
+              <hr/>              
+           </div>     
+
+          </div>
+        </div>
+        
       </div>
 
       {/*Languages & Tools */}
